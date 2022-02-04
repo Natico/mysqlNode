bd1174da21ab54a066242f7921a14794d34bd7fc
Nasser Amjadian <amjadian.n@gmail.com>
2022-02-04 19:38:02 +0330

    add freemysqlhosting.net database to config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ module.exports = {
     //     database: 'nodemysql',
     // }
     database: {
-        host: 'testable.ir',
-        user: 'testable_admin',
-        password: 'vEC81$g9',
-        database: 'testable_prestashop',
+        host: 'sql6.freemysqlhosting.net',
+        user: 'sql6470425',
+        password: '6YymWDyaDs',
+       
      
     }
 }
